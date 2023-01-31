cfcd8cda6a089d587a2658ec74d36a9e479931bc
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-31 23:58:14 +0700

    modified: responsive About - HomePage

--- src/parts/HomePage/About.jsx
+++ src/parts/HomePage/About.jsx
@@ -8,18 +8,18 @@ import AboutImg from "../../assets/images/about-img.png";
 export default function About() {
   return (
     <section className="about section">
-      <div className="about__container container grid gap-12">
+      <div className="about__container container grid gap-12 xs:max-w-sm sm:max-w-md md:max-w-lg xl:max-w-4xl xl:grid-cols-2 xl:items-center 2xl:max-w-5xl">
         <div className="about__data">
           <p className="about__subtitle section-subtitle">About Us</p>
           <h1 className="about__title section-title">Explore world with us</h1>
-          <p className="about__text section-text pb-8">
+          <p className="about__text section-text pb-8 2xl:max-w-md">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla
             veniam nostrum numquam ipsa laboriosam eveniet voluptatum illo
             corrupti impedit ex sit.
           </p>
           <Link to="/" className="about__button button inline-flex">
             Get Started
-            <RiArrowRightLine />
+            <RiArrowRightLine className="text-[1.3rem]" />
           </Link>
         </div>
 
