e32fe2588bb3d4927fc2df31df37a741778c825d
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-31 19:02:51 +0700

    modified: responsive Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,6 @@ export default function Header() {
   // open menu state
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const setHandleMenu = () => {
-    setMenuOpen((even) => !even);
-  };
-
   return (
     <header className="header fixed top-0 left-0 z-50 w-full">
       <div className="header__container container flex h-24 items-center justify-between">
@@ -23,11 +19,11 @@ export default function Header() {
         </Link>
 
         <div
-          className={`header__menu fixed top-24 left-0 w-full origin-top bg-white py-8 px-16 text-center shadow-md transition-all duration-400 ${
+          className={`header__menu fixed top-24 left-0 w-full origin-top bg-white py-8 px-16 text-center shadow-md transition-all duration-400 md:static md:top-0 md:flex md:w-auto md:scale-y-100 md:items-center md:gap-8 md:p-0 md:shadow-none ${
             menuOpen ? "scale-y-100" : "scale-y-0"
           }`}
         >
-          <ul className="header__list mb-8 flex flex-col gap-8">
+          <ul className="header__list mb-8 flex flex-col gap-8 md:mb-0 md:flex-row">
             {[
               ["About", "/about"],
               ["Why Us", "/why-us"],
@@ -53,8 +49,8 @@ export default function Header() {
         </div>
 
         <div
-          className="header__toggle inline-flex cursor-pointer p-1 text-[1.3rem] text-gray-900"
-          onClick={setHandleMenu}
+          className="header__toggle inline-flex cursor-pointer p-1 text-[1.3rem] text-gray-900 md:hidden"
+          onClick={() => setMenuOpen(!menuOpen)}
         >
           {!menuOpen ? <RiMenu3Fill /> : <RiCloseFill />}
         </div>
