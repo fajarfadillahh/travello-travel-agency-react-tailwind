dccef10f2df6b853be2299cd66e191e227ba3117
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-01 16:30:35 +0700

    create: custom hook useLocalStorage & useColorMode | modified: activated dark mode at Header Components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,9 @@ import {
   RiSunFill,
 } from "react-icons/ri";
 
+// import color mode custom hooks
+import useColorMode from "../hooks/useColorMode";
+
 export default function Header() {
   // open menu state
   const [menuOpen, setMenuOpen] = useState(false);
@@ -26,24 +29,31 @@ export default function Header() {
     });
   }, [stickyHeader]);
 
+  // color mode state
+  const [colorMode, setColorMode] = useColorMode();
+
   return (
     <header
       className={`header fixed top-0 left-0 z-50 w-full transition-all duration-400 ${
-        stickyHeader ? "bg-white shadow-md" : "bg-transparent shadow-none"
+        stickyHeader
+          ? "bg-white shadow-md dark:bg-gray-900"
+          : "bg-transparent shadow-none"
       }`}
     >
       <div className="header__container container flex h-24 items-center justify-between">
+        {/* header logo */}
         <Link
           to="/"
-          className={`header__logo font-serif text-[20px] font-semibold ${
+          className={`header__logo font-serif text-[20px] font-semibold dark:text-white ${
             stickyHeader ? "text-gray-900" : "text-white"
           }`}
         >
           Travello.
         </Link>
 
+        {/* header menu */}
         <div
-          className={`header__menu fixed top-0 -z-10 flex h-full w-[70%] flex-col justify-between bg-white px-10 pt-24 pb-10 shadow-[0_-4px_12px_rgba(0,0,0,0.12)] transition-all duration-400 md:static md:z-auto md:h-auto md:w-auto md:flex-row md:items-center md:gap-8 md:bg-transparent md:p-0 md:shadow-none ${
+          className={`header__menu fixed top-0 z-10 flex h-full w-[70%] flex-col justify-between bg-white px-10 pt-24 pb-10 shadow-[0_-4px_12px_rgba(0,0,0,0.12)] transition-all duration-400 dark:bg-gray-900 md:static md:z-auto md:h-auto md:w-auto md:flex-row md:items-center md:gap-8 md:bg-transparent md:p-0 md:shadow-none md:dark:bg-transparent ${
             menuOpen ? "right-0" : "-right-full"
           }`}
         >
@@ -58,7 +68,7 @@ export default function Header() {
               <li key={url}>
                 <Link
                   to={url}
-                  className={`header__link group relative text-[15px] font-medium leading-tight text-gray-900 md:text-white ${
+                  className={`header__link group relative text-[15px] font-medium leading-tight text-gray-900 dark:text-white md:text-white ${
                     stickyHeader ? "md:text-gray-900" : "md:text-white"
                   }`}
                 >
@@ -75,11 +85,14 @@ export default function Header() {
 
           {/* header theme toggle [desktop version] */}
           <div
-            className={`header__theme hidden cursor-pointer p-1 text-[1.3rem] md:flex ${
+            className={`header__theme hidden cursor-pointer p-1 text-[1.3rem] dark:text-white md:flex ${
               stickyHeader ? "text-gray-900" : "text-white"
             }`}
+            onClick={() =>
+              setColorMode(colorMode === "light" ? "dark" : "light")
+            }
           >
-            <RiMoonFill />
+            {colorMode === "light" ? <RiMoonFill /> : <RiSunFill />}
           </div>
 
           <Link to="/register" className="header__button button">
@@ -90,24 +103,29 @@ export default function Header() {
         <div className="header__wrapper inline-flex items-center gap-5 md:hidden">
           {/* header theme toggle [mobile version] */}
           <div
-            className={`header__theme cursor-pointer p-1 text-[1.3rem] ${
+            className={`header__theme cursor-pointer p-1 text-[1.3rem] dark:text-white ${
               stickyHeader ? "text-gray-900" : "text-white"
             }`}
+            onClick={() =>
+              setColorMode(colorMode === "light" ? "dark" : "light")
+            }
           >
-            <RiMoonFill />
+            {colorMode === "light" ? <RiMoonFill /> : <RiSunFill />}
           </div>
 
           {/* header menu toggle */}
           <div
-            className="header__toggle inline-flex cursor-pointer p-1 text-[1.3rem]"
+            className="header__toggle z-10 inline-flex cursor-pointer p-1 text-[1.3rem]"
             onClick={() => setMenuOpen(!menuOpen)}
           >
             {!menuOpen ? (
               <RiMenu3Fill
-                className={`${stickyHeader ? "text-gray-900" : "text-white"}`}
+                className={`dark:text-white ${
+                  stickyHeader ? "text-gray-900 " : "text-white"
+                }`}
               />
             ) : (
-              <RiCloseFill className="text-gray-900" />
+              <RiCloseFill className="text-gray-900 dark:text-white" />
             )}
           </div>
         </div>
