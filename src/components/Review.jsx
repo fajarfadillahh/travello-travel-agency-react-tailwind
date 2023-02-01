db0b79f4b935202fe51382eedcf10bf05afeccb3
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-01 12:35:40 +0700

    modified: responsive Review components

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -7,7 +7,7 @@ import ReviewImg from "../assets/images/review-img.png";
 export default function Review() {
   return (
     <section className="review section">
-      <div className="review__container container grid gap-12">
+      <div className="review__container container grid gap-12 xs:max-w-sm sm:max-w-md md:max-w-lg xl:max-w-4xl xl:grid-cols-2 xl:items-center 2xl:max-w-5xl">
         <div className="review__data">
           <p className="review__subtitle section-subtitle">Whay they say</p>
           <h1 className="review__title section-title">Customer reviews</h1>
