d76b6376f556e43a5dda9806a2765b4955d2932a
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-01 16:41:31 +0700

    modified: activated dark mode at NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <section className="flex h-screen w-full items-center justify-center">
+    <section className="flex h-screen w-full items-center justify-center transition-all duration-400 dark:bg-gray-900">
       <div className="container text-center">
         <h1 className="section-title">404 Not Found</h1>
         <p className="section-text pb-8">
