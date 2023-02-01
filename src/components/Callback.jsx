dce61758d12d557d5cb95646647740f289524dba
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-01 12:46:50 +0700

    create: Callback components

---
+++ src/components/Callback.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// import icons
+import { RiArrowRightLine } from "react-icons/ri";
+
+// import images
+import CallbackImg from "../assets/images/callback-img.png";
+
+export default function Callback() {
+  return (
+    <section className="callback section">
+      <div className="callback__wrapper relative flex h-[640px] items-center">
+        <img
+          src={CallbackImg}
+          alt="callback img"
+          className="callback__img absolute top-0 left-0 h-full w-full object-cover object-center"
+        />
+
+        {/* overlay */}
+        <div className="absolute top-0 left-0 h-full w-full bg-gray-900/30" />
+
+        <div className="callback__container container">
+          <div className="callback__data relative text-center">
+            <p className="callback__subtitle section-subtitle text-white">
+              Let's talk
+            </p>
+            <h1 className="callback__title section-title text-white">
+              Contact us if you're ready
+            </h1>
+            <p className="callback__text section-text pb-8 text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              itaque eaque cumque libero dignissimos aut maiores odit commodi
+              aspernatur expedita.
+            </p>
+            <Link to="/contact" className="callback__button button inline-flex">
+              Contact us
+              <RiArrowRightLine className="text-[1.3rem] text-white" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import About from "../parts/HomePage/About";
 import Service from "../parts/HomePage/Service";
 import Destinations from "../parts/HomePage/Destinations";
 import Review from "../components/Review";
+import Callback from "../components/Callback";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <Service />
       <Destinations />
       <Review />
+      <Callback />
     </>
   );
 }
